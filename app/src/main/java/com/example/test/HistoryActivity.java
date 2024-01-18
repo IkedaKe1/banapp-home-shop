558edea34c773f43dd705d67d160cba8150f20fe
IkedaKe1 <san-j22003@sist.ac.jp>
2024-01-18 22:40:42 +0900

    version 2.1 修正内容: ・アチーブメント画面結合 ・ヒストリー画面結合 ・ホーム、吸っていないかの確認画面、ショップ画面にコメント追加

--- app/src/main/java/com/example/test/HistoryActivity.java
+++ app/src/main/java/com/example/test/HistoryActivity.java
@@ -13,6 +13,16 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
+        //ボタンを押した時
+        findViewById(R.id.bt_test_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //インテントの追加（これが遷移用ロジックです）
+                Intent intent = new Intent(HistoryActivity.this, StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
         //戻るボタン処理
         findViewById(R.id.bt_Back_history).setOnClickListener(new View.OnClickListener() {
             @Override
